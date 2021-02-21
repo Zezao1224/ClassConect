8444ca82652813742691a3ae7c349a699b0fd656
Bruno Luiz <gamer@PCZezao.sa-east-1.ec2-utilities.amazonaws.com>
2021-02-21 18:54:44 -0300

    showresultsarray

--- P_Conexao/Program.vb
+++ P_Conexao/Program.vb
@@ -10,13 +10,19 @@ Module Program
         If conect.Conect() = True Then
             table = conect.Tabela()
             dtr = table.Select()
-            For j As Integer = 1 To dtr(0).ItemArray.Length()
-                Console.WriteLine(table.Columns(j))
+            For j As Integer = 0 To table.Columns.Count() - 1
+                txt = txt & "|" & table.Columns(j).ToString()
             Next
-            For j As Integer = 0 To dtr(0).ItemArray.Length()
-                For i As Integer = 0 To dtr.Length()
-                    txt = txt & "" & dtr(j).ItemArray(i)
+            Console.WriteLine(txt)
+            txt = ""
+            'coluna'
+            For j As Integer = 0 To dtr.Length() - 1
+                'linha'
+                For i As Integer = 0 To dtr(0).ItemArray.Length() - 1
+                    txt = txt & "|" & dtr(j).ItemArray(i)
                 Next
+                Console.WriteLine(txt)
+                txt = ""
             Next
         Else
             Console.WriteLine()
